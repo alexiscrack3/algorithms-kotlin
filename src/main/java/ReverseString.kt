09366bbad53924d246e92bc36c7a5b10788c13ce
Alexis Ortega <alexis.oc3@gmail.com>
2020-11-05 21:30:23 -0700

    Reverse string

--- src/main/java/ReverseString.kt
+++ src/main/java/ReverseString.kt
@@ -1,6 +1,20 @@
+import java.lang.StringBuilder
+
 class ReverseString {
 
     fun reverse(input: String): String {
-        return input.reversed()
+        val output = StringBuilder()
+        for (char in input) {
+            if (char.isLetter() || char.isDigit()) {
+                output.insert(0, char)
+            }
+        }
+        for (char in input) {
+            if (char.isLetter().not() && char.isDigit().not()) {
+                val index = input.indexOf(char)
+                output.insert(index, char)
+            }
+        }
+        return output.toString()
     }
 }
--- src/test/java/ReverseStringTest.kt
+++ src/test/java/ReverseStringTest.kt
@@ -5,7 +5,17 @@ import org.junit.Test
 class ReverseStringTest {
 
     @Test
-    fun `Hello World!`() {
+    fun `given "a,b$c" when string is reversed then "c,b$a"`() {
+        val input = "a,b\$c"
+        val testObject = ReverseString()
+
+        val output = testObject.reverse(input)
+
+        assertThat(output, equalTo("c,b\$a"))
+    }
+
+    @Test
+    fun `given "Ab,c,de!$" when string is reversed then "ed,c,bA!$"`() {
         val input = "a,b\$c"
         val testObject = ReverseString()
 
