427d80faeab1e11ad75266c2b567e42513490d73
Alexis Ortega <alexis.oc3@gmail.com>
2020-11-06 09:05:28 -0700

    Implement best solution to reverse string

--- src/main/java/ReverseString.kt
+++ src/main/java/ReverseString.kt
@@ -1,8 +1,27 @@
-import java.lang.StringBuilder
 
 class ReverseString {
 
     fun reverse(input: String): String {
+        val arr = Array(input.length) { input[it] }
+        var start = 0
+        var end = input.length - 1
+        while (start < end) {
+            if (input[start].isLetter().not()) {
+                start++
+            } else if (input[end].isLetter().not()) {
+                end--
+            } else {
+                val temp = input[start]
+                arr[start] = arr[end]
+                arr[end] = temp
+                start++
+                end--
+            }
+        }
+        return arr.joinToString("")
+    }
+
+    fun reverseAlt(input: String): String {
         val output = StringBuilder()
         for (char in input) {
             if (char.isLetterOrDigit()) {
